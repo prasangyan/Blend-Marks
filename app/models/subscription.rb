da055fb826f0abc8c736f55d8c14ec87ca42ee6c
Santhosh PS <santhoshonet@gmail.com>
2011-04-07 12:20:49 +0530

    implemented email validation with + sign

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,6 @@
 class Subscription < ActiveRecord::Base
 
   validates_presence_of :email, :message => "input email address."
-  #validates_format_of :email , :with => %r{^(?:[_a-z0-9-]+)(\.[_a-z0-9-]+)*@([a-z0-9-]+)(\.[a-zA-Z0-9\-\.]>> +)*(\.[a-z]{2,4})$}i, :message => "invalid email format."
+  validates_format_of :email, :with => %r{^(?:[_a-z0-9|+-]+)(\.[_a-z0-9|+-]+)*@([a-z0-9-]+)(\.[a-zA-Z0-9\-\.]>> +)*(\.[a-z]{2,4})$}i, :message => "invalid email format."
 
 end
