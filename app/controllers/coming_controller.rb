2b5724fbdb11df15bf547ee83aa1a6bb5523db8e
Santhosh PS <santhoshonet@gmail.com>
2011-04-07 11:47:12 +0530

    modified layout and removed email format validation.

--- app/controllers/coming_controller.rb
+++ app/controllers/coming_controller.rb
@@ -3,14 +3,16 @@ class ComingController < ApplicationController
   def index
     @subscription = Subscription.new
     @message = nil
+    @success = false
   end
 
   def subscribe
-
+    @success = false
     sub = Subscription.new
     sub.email = params[:email]
     if sub.save
       @message = "Thank you. We will send you an update as soon as we are ready"
+      @success = true
     else
       sub.errors.each do |key,value|
         @message = value
