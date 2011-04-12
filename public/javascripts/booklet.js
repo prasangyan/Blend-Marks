246b299f2ea735fff9bc0657a94cf2584ee9f44d
Santhosh PS <santhoshonet@gmail.com>
2011-04-12 18:24:27 +0530

    modified url

--- public/javascripts/booklet.js
+++ public/javascripts/booklet.js
@@ -1,27 +1,2 @@
-function request() {
-            var ajaxRequest;
-            try {
-                ajaxRequest = new XMLHttpRequest();
-            }
-            catch (e) {
-                try {
-                    ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
-                } catch (e) {
-                    try {
-                        ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
-                    }
-                    catch (e) {
-                        alert("your browser breaks this functionality!"); return false;
-                    }
-                }
-            }
-            ajaxRequest.onreadystatechange = function () {
-                if (ajaxRequest.readyState == 2) {
-                    alert("BlendMark added");
-                }
-            };
-            var queryString = "http://blendmarks.heroku.com/quickentry?url=" + window.location;
-            ajaxRequest.open('GET', queryString, true);
-            ajaxRequest.open('POST', queryString, true);
-       };
-request();
+try{var iframe=document.createElement('iframe');iframe.src="http://blendmarks.heroku.com/quickentry?url=" + window.location;document.body.appendChild(iframe);alert("BlendMark added");}catch(e){alert(e.message);
+}
